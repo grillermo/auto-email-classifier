b99f5de0509389444d412b16f654714ccac428ed
Guillermo Siliceo <guillermo.siliceo@gmail.com>
2026-05-29 14:06:23 -0600

    Fix hosts

--- config/application.rb
+++ config/application.rb
@@ -42,5 +42,6 @@ module RulesEditor
     config.active_record.encryption.primary_key = ENV.fetch("AR_ENCRYPTION_PRIMARY_KEY", nil)
     config.active_record.encryption.deterministic_key = ENV.fetch("AR_ENCRYPTION_DETERMINISTIC_KEY", nil)
     config.active_record.encryption.key_derivation_salt = ENV.fetch("AR_ENCRYPTION_KEY_DERIVATION_SALT", nil)
+    config.hosts << "auto-email-classifier.chiq.me"
   end
 end
